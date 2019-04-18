d261d776b0274c08437872c332afdb59445186ea
only2433 <only2433@naver.com>
2019-04-18 11:38:33 +0900

    Init Commit

---
+++ southStreetSchool/src/main/java/com/littlefox/storybook/southstreetschool/MainApplication.java
@@ -0,0 +1,15 @@
+package com.littlefox.storybook.southstreetschool;
+
+import android.app.Application;
+import android.util.DisplayMetrics;
+
+public class MainApplication extends Application{
+	
+	@Override
+    public void onCreate()
+    {
+        super.onCreate();
+    }
+	
+	
+}
